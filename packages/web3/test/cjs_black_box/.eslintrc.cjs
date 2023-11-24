708caa8da0332e839563d771fb407e6d173f8092
Jessica Bennett <Jessbennett924@gmail.com>
2023-11-23 20:20:14 -0500

    Update .eslintrc.cjs
    Bank music int

--- packages/web3/test/cjs_black_box/.eslintrc.cjs
+++ packages/web3/test/cjs_black_box/.eslintrc.cjs
@@ -1,12 +1,1 @@
-module.exports = {
-	env: {
-		node: true,
-		commonjs: true,
-		es2021: true,
-	},
-	parserOptions: {
-		project: './tsconfig.json',
-		tsconfigRootDir: __dirname,
-		ecmaVersion: 12
-	},
-};
+#R
